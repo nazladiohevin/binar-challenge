08ba5e235746fa3a2d8c688093f76d799c96916e
nazladiohevin <nazlahevinbusiness@gmail.com>
2024-09-26 21:35:02 +0700

    feat: flight function

--- flight.js
+++ flight.js
@@ -1,1 +1,27 @@
 // To Do
+
+class FlightError extends Error {
+  constructor(message) {
+      super(message);
+      this.name = 'FlightError';
+  }
+}
+
+class EngineError extends Error {
+  constructor(message) {
+      super(message);
+      this.name = 'EngineError';
+  }
+}
+
+function flight(latitude) {
+  if (Math.random() > 0.75) {
+      throw new EngineError('Engine is not working');
+  }
+  // 3000 < latitude < 10000
+  if (latitude > 10000 || latitude < 3000) {
+      console.log('Plane is flying at latitude: ', latitude);
+      throw new FlightError('Cannot fly at this latitude');
+  }
+  console.log('Flying at latitude: ', latitude);
+}
